daf7620feb4a21748a072ab3bdf085b7b041b185
dbelshaw <david.belshaw@nice.org.uk>
2016-07-20 16:16:27 +0100

    implementing new Design structure

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const express = require('express'),
 PORT = 1234,
 app = express(),
 jsonQuery = require('json-query'),
-fs = require("fs");
+fs = require("fs"),
+groupBy = require('group-by');
 
 // Tells express we want to use jade and where we will keep our views
 app.set('views', __dirname + '/views/')
@@ -16,46 +17,20 @@ app.get('/', function (req, res) {
   var categories = jsonQuery('categories[]', {
         data: json
     });
+  //var statusTypes = jsonQuery('categories[name=Languages and Frameworks]', {
+     //   data: obj
+    //}).value;
+   // var techs = groupBy(query["values"], "status");
 
   res.render('index',
   { 
     title : 'NICE Tech Radar',
     categories : categories.value,
+
     }
   )
 });
 
-app.get('/languages-and-frameworks', function (req, res) {
-    var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-    var query = jsonQuery('categories[name=Languages and Frameworks]', {
-        data: obj
-    }).value;
-    var values = query["values"];
-  res.render('category',
-  { 
-    title : 'Languages and Frameworks',
-    category : query["name"],
-    catslug : query["slugName"],
-    json : query["values"]
-    }
-  )
-})
-
-app.get('/platforms-and-infrastructure', function (req, res) {
-    var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-    var query = jsonQuery('categories[name=Platforms and Infrastructure]', {
-        data: obj
-    }).value;
-    var values = query["values"];
-  res.render('category',
-  { 
-    title : 'Platforms and Infrastructure',
-    category : query["name"],
-    catslug : query["slugName"],
-    json : query["values"]
-    }
-  )
-})
 
 app.listen(PORT);
 
