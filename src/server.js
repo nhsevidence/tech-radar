1b62970447c859cbab08f286b3c5a9a06f6240ab
dbelshaw <david.belshaw@nice.org.uk>
2016-07-20 12:28:25 +0100

    Creating landing page which outputs categories

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 'use strict';
 
-const express = require('express');
-const PORT = 1234;
-const app = express();
+const express = require('express'),
+PORT = 1234,
+app = express(),
+jsonQuery = require('json-query'),
+fs = require("fs");
 
+// Tells express we want to use jade and where we will keep our views
+app.set('views', __dirname + '/views/')
+app.set('view engine', 'jade')
+
+// Our base url 
 app.get('/', function (req, res) {
-  res.send('Hello, Im the tech radar app\n');
+  var json = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  var categories = jsonQuery('categories', {
+        data: json
+    });
+
+  res.render('index',
+  { 
+    title : 'NICE Tech Radar',
+    categories : categories.value,
+    }
+  )
 });
 
 app.listen(PORT);
