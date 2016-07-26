c5576c8f8ee0ef5a63ce722596ce5aa4a4a2ae19
dbelshaw <david.belshaw@nice.org.uk>
2016-07-26 13:50:50 +0100

    fix bug when clicking on tech

--- src/public/scripts/tech-radar.js
+++ src/public/scripts/tech-radar.js
@@ -14,6 +14,8 @@ $( document ).ready(function() {
             url: "/technology/" + url,
             success: function (data) {
                 $('.desc-container').html(data);
+                 var scrollVal = $(window).scrollTop();
+                CheckScrollState(scrollVal);
             }
         });
     });
@@ -21,10 +23,14 @@ $( document ).ready(function() {
      $(window).on("scroll", function() {
         //this will calculate header's full height, with borders, margins, paddings
        var scrollVal = $(this).scrollTop();     
+        CheckScrollState(scrollVal);
+    });
+
+    function CheckScrollState(scrollVal) {
         if ( scrollVal > 140 ) {
             $('#desc').addClass("fixed");
         } else {
             $('#desc').removeClass("fixed");
         }
-    });
+    }
 });
