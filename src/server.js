e8493eea44fae486fa087c315f0a0d5b4c657044
dbelshaw <david.belshaw@nice.org.uk>
2016-07-28 09:50:47 +0100

    Refactoring

--- src/server.js
+++ src/server.js
@@ -32,31 +32,38 @@ var env = nunjucks.configure(path.join( __dirname, "/views") , {
 app.use(express.static(__dirname + '/public'))
 // Our base url 
 app.get('/', function (req, res) {
-    var techData = jsonQuery('categories[]values[url=' + req.query.desc + ']', {
-        data: json
-      });
-   var category = techData.value != null ? techData.parents[2].value.name : null;
+  var tech = GetTech(req.query.desc);
 
   var data = {
       categories : mappedCategories,
       statusList : viewStatusList,
-      tech : techData.value,
-      category: category
+      tech :  tech.data,
+      category: tech.category
     }
   radar(res, data);
 });
 
 app.get('/technology/:tech', function (req, res) {
-        var techData = jsonQuery('categories[]values[url=' + req.params.tech + ']', {
+      var tech = GetTech(req.params.tech);
+      res.render('partials/description',
+    { 
+      tech :  tech.data,
+      category: tech.category
+    });
+});
+
+function GetTech(tech)
+{
+    var techData = jsonQuery('categories[]values[url=' + tech + ']', {
         data: json
     });
     var category = techData.value != null ? techData.parents[2].value.name : null;
-      res.render('partials/description',
-    { 
-      tech :  techData.value,
-      category: category
-    });
-});
+    var tech = {
+      data : techData.value,
+      category : category
+    }
+    return tech;
+}
 
 
 app.listen(PORT);
