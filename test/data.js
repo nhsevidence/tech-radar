38af3e717cee977fff2681ab733a1b7b25b68f0b
James Kirk <james.kirk84@gmail.com>
2016-07-28 17:14:48 +0100

    adding test

--- test/data.js
+++ test/data.js
@@ -4,74 +4,103 @@ var assert = require("assert"); // node.js core module
 var dataMod = require("../src/data");
 
 describe('Tech radar', function(){
-    describe('Data', function() {
+    describe('When reading the data', function() {
         it('should apply the status name to each technology using id as lookup', function(){
 
-        var statusList = [
-            {
-            id : 1,
-            name : 'Assessing'
-            }
-        ];
-
-        var categories = [
-            {               
-            values : [
-                { 
-                    status : 1
+            var statusList = [
+                {
+                id : 1,
+                name : 'Assessing'
                 }
-            ]
-            }
-        ];
-        
+            ];
 
-        var viewModel = dataMod(categories, statusList);
+            var categories = [
+                {               
+                values : [
+                    { 
+                        status : 1
+                    }
+                ]
+                }
+            ];
+            
 
-        assert.equal(viewModel[0].values[0].status, 'Assessing');
+            var viewModel = dataMod(categories, statusList);
+
+            assert.equal(viewModel[0].values[0].status, 'Assessing');
         });
 
 
-         it('should order the tech by status', function(){
+        it('should order the tech by status', function(){
 
-        var statusList = [
-            {
-            id : 1,
-            name : 'Assessing'
-            },{
-            id : 2,
-            name : 'Trialling'
-            },
-            {
-            id : 3,
-            name : 'Adopted'
-            }
-        ];
-
-        var categories = [
-            {               
-            values : [
-                { 
-                    name: "Second",
-                    status : 2
+            var statusList = [
+                {
+                    id : 1
                 },
-                 { 
-                    name: "Third",
-                    status : 3
+                {
+                    id : 2
                 },
-                 { 
-                    name: "First",
-                    status : 1
+                {
+                    id : 3
                 }
-            ]
-            }
-        ];
+            ];
+
+            var categories = [
+                {               
+                    values : [
+                        { 
+                            name: "Second",
+                            status : 2
+                        },
+                         { 
+                            name: "Third",
+                            status : 3
+                        },
+                         { 
+                            name: "First",
+                            status : 1
+                        }
+                    ]
+                }
+            ];
+            
+
+            var viewModel = dataMod(categories, statusList);
+
+            assert.equal(viewModel[0].values[0].name, 'First');
+            assert.equal(viewModel[0].values[1].name, 'Second');
+            assert.equal(viewModel[0].values[2].name, 'Third');
+        });
         
+        it('should order the tech alphabetically by name within each status', function(){
 
-        var viewModel = dataMod(categories, statusList);
+            var statusList = [
+                {
+                    id : 1,
+                    name : ''
+                }
+            ];
 
-        assert.equal(viewModel[0].values[0].name, 'First');
-        assert.equal(viewModel[0].values[1].name, 'Second');
-        assert.equal(viewModel[0].values[2].name, 'Third');
+            var categories = [
+                {               
+                    values : [
+                        { 
+                            name: "B",
+                            status : 1
+                        },
+                        { 
+                            name: "A",
+                            status : 1
+                        }
+                    ]
+                }
+            ];
+            
+
+            var viewModel = dataMod(categories, statusList);
+
+            assert.equal(viewModel[0].values[0].name, 'A');
+            assert.equal(viewModel[0].values[1].name, 'B');
         });
   });
 });
