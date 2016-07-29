495ea63a6d301a197a000d1e8371c6621e152694
James Kirk <james.kirk84@gmail.com>
2016-07-29 12:45:09 +0100

    fixing bookmarked tech

--- src/server.js
+++ src/server.js
@@ -28,11 +28,27 @@ app.use(express.static(__dirname + '/public'))
 var templatePath = path.join( __dirname, "/views")
 var renderFn = renderSetup(templatePath);
 
+function GetTech(techUrl)
+{
+    var techData = jsonQuery('categories[]values[url=' + techUrl + ']', {
+        data: json
+    });
+    var category = techData.value != null ? techData.parents[2].value.name : null;
+    var tech = {
+      data : techData.value,
+      category : category
+    }
+    return tech;
+}
+
 // Our base url 
 app.get('/', function (req, res) {
+  var tech = GetTech(req.query.desc);
   var data = {
       categories : mappedCategories,
-      statusList : viewStatusList
+      statusList : viewStatusList,
+      tech :  tech.data,
+      category: tech.category
     };
 
   var html = radar(renderFn, data);
